b415d56543e7b61e9769aaa707c2b60b4852f83f
Ultimate <ab3854093@gmail.com>
2026-02-26 10:54:58 +0200

    add screen complete_info_item.dart and complete_information_view.dart and complete_information_view_body.dart

---
+++ lib/Features/login/presenation/complete_information/complete_information_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:untitled2/Features/login/presenation/complete_information/widgets/complete_information_view_body.dart';
+
+class CompleteInformationView extends StatelessWidget {
+  const CompleteInformationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(body: CompleteInformationBody()));
+  }
+}
