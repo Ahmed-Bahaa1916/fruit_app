f44fad3f76a07154fbd35085ef25d604321e484b
Ultimate <ab3854093@gmail.com>
2026-02-26 13:17:13 +0200

    add firebase in project and build.gradle.kts

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'Features/splash/presentation/splash_view.dart';
-
-void main() {
-  runApp(const FruitApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FruitApp());
 }
 
 class FruitApp extends StatelessWidget {
@@ -20,5 +22,3 @@ class FruitApp extends StatelessWidget {
     ));
   }
 }
-
-// layoutBuilder
